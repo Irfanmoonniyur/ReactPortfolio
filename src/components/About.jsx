790f42ff062959eca9c79937ad8d113ac149a180
Irfanmoonniyur <109480063+Irfanmoonniyur@users.noreply.github.com>
2023-07-26 16:35:23 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,14 +7,19 @@ const About = () => {
             <div className="pb-8">
                 <p className="text-4xl font-bold inline border-b-4 border-gray-500">About</p>
             </div>
-            <p className='text-xl mt-20'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque, asperiores! Ut numquam 
-                incidunt beatae deleniti vel in sunt, ea culpa distinctio ducimus officiis sequi, dolorem voluptatum
-                 aut quam commodi labore.</p>
+            <p className='text-xl mt-20'>Hello, I'm Mohamed Irfan - a versatile individual who embraces the fusion of chemical engineering and web development. 
+              My educational background in chemical engineering has equipped me with strong problem-solving skills, analytical thinking, and an eye for detail. 
+              Alongside my studies, I discovered my passion for coding and web development, leading me to explore the digital world with equal enthusiasm.
+              Merging the precision of engineering with the creativity of web development, I offer a unique perspective 
+              and approach to every project I undertake..</p>
             <br/>
-            <p className='text-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                Voluptates sit dolor veritatis voluptatem eligendi voluptas possimus est, 
-                perferendis consequuntur rem nobis similique ut quisquam soluta veniam 
-                numquam incidunt magnam quidem?</p>
+            <p className='text-xl'>
+            As a web developer with a chemical engineering background, I am driven by the desire to create efficient and innovative digital solutions.
+              My engineering training enables me to analyze complex problems systematically and devise optimized strategies. 
+              Simultaneously, my coding expertise empowers me to turn these ideas into reality through intuitive and user-friendly websites and applications.
+              I relish the opportunity to bridge the gap between two seemingly distinct domains, finding inspiration in the synergy between them. With a focus on continuous learning and pushing my boundaries, I am excited to contribute my diverse skills to deliver outstanding results and contribute positively to the digital landscape
+            
+            </p>
            
         </div>
     </div>
